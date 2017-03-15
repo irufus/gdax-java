b4797200fa3a875dd2e4195d56d89875fd7c9ae4
Rob Evans <rwevans1984@gmail.com>
2017-03-15 16:48:47 +0000

    started implementing a default list model so I can add bids/asks to it - needs more work

--- src/main/java/com/coinbase/exchange/api/gui/orderbook/OrderBookView.java
+++ src/main/java/com/coinbase/exchange/api/gui/orderbook/OrderBookView.java
@@ -2,12 +2,12 @@ package com.coinbase.exchange.api.gui.orderbook;
 
 import com.coinbase.exchange.api.marketdata.MarketData;
 import com.coinbase.exchange.api.marketdata.MarketDataService;
-import com.coinbase.exchange.api.orders.Order;
 import com.coinbase.exchange.api.websocketfeed.WebsocketfeedService;
-import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.swing.*;
+import java.math.BigDecimal;
+import java.util.Arrays;
 
 /**
  * Created by robevansuk on 10/03/2017.
@@ -21,28 +21,26 @@ public class OrderBookView extends JPanel {
     WebsocketfeedService websocketFeed;
 
     JList<String> bids = new JList<>();
-    JList<String> sells = new JList<>();
+    JList<String> asks = new JList<>();
 
     public OrderBookView(){
         super();
         String[] productIds = new String[] { "BTC-GBP", "BTC-ETH" }; // make this configurable.
         for (String productId : productIds) {
-            MarketData marketData = marketDataService.getMarketDataOrderBook(productId, "1");
-            initBids();
-            initAsks();
+            MarketData marketData = marketDataService.getMarketDataOrderBook(productId, "2");
+            this.add(initJList(bids, marketData));
+            this.add(initJList(asks, marketData));
         }
 
         // start live feed(s)
         websocketFeed.openWebsocketFeed(productIds);
     }
 
-    private void initAsks() {
+    private JList initJList(JList list, MarketData marketData) {
         // init the asks JList
-    }
-
-
-    private void initBids() {
-        // init the bids JList
+        BigDecimal[] priceList = Arrays.stream(marketData.getAsks()));
+        ListModel model = new DefaultComboBoxModel(priceList);
+        list.setModel(model);
     }
 
     public void newTrade(String msg) {
