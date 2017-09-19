0ab87e4c2bb80be0d4e0aa8de2a4c63b5bee74c3
Duane May <djmay@mayhoo.com>
2017-09-18 21:40:40 -0400

    Add toString method

--- src/main/java/com/coinbase/exchange/api/orders/Order.java
+++ src/main/java/com/coinbase/exchange/api/orders/Order.java
@@ -3,7 +3,8 @@ package com.coinbase.exchange.api.orders;
 /**
  * Created by robevansuk on 03/02/2017.
  */
-public class Order {
+public class Order
+{
     String id;
     String size;
     String price;
@@ -19,9 +20,12 @@ public class Order {
     String executed_value;
     String status;
 
-    public Order() {}
+    public Order()
+    {
+    }
 
-    public Order(String id, String price, String size, String product_id, String side, String stp, String type, String time_in_force, String post_only, String created_at, String fill_fees, String filled_size, String executed_value, String status, Boolean settled) {
+    public Order(String id, String price, String size, String product_id, String side, String stp, String type, String time_in_force, String post_only, String created_at, String fill_fees, String filled_size, String executed_value, String status, Boolean settled)
+    {
         this.id = id;
         this.price = price;
         this.size = size;
@@ -41,7 +45,8 @@ public class Order {
 
     Boolean settled;
 
-    public Order(OrderBuilder builder) {
+    public Order(OrderBuilder builder)
+    {
         this.id = builder.getId();
         this.size = builder.getSize();
         this.price = builder.getPrice();
@@ -54,132 +59,167 @@ public class Order {
         this.created_at = builder.getCreated_at();
     }
 
-    public String getId() {
+    public String getId()
+    {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(String id)
+    {
         this.id = id;
     }
 
-    public String getPrice() {
+    public String getPrice()
+    {
         return price;
     }
 
-    public void setPrice(String price) {
+    public void setPrice(String price)
+    {
         this.price = price;
     }
 
-    public String getSize() {
+    public String getSize()
+    {
         return size;
     }
 
-    public void setSize(String size) {
+    public void setSize(String size)
+    {
         this.size = size;
     }
 
-    public String getProduct_id() {
+    public String getProduct_id()
+    {
         return product_id;
     }
 
-    public void setProduct_id(String product_id) {
+    public void setProduct_id(String product_id)
+    {
         this.product_id = product_id;
     }
 
-    public String getSide() {
+    public String getSide()
+    {
         return side;
     }
 
-    public void setSide(String side) {
+    public void setSide(String side)
+    {
         this.side = side;
     }
 
-    public String getStp() {
+    public String getStp()
+    {
         return stp;
     }
 
-    public void setStp(String stp) {
+    public void setStp(String stp)
+    {
         this.stp = stp;
     }
 
-    public String getType() {
+    public String getType()
+    {
         return type;
     }
 
-    public void setType(String type) {
+    public void setType(String type)
+    {
         this.type = type;
     }
 
-    public String getTime_in_force() {
+    public String getTime_in_force()
+    {
         return time_in_force;
     }
 
-    public void setTime_in_force(String time_in_force) {
+    public void setTime_in_force(String time_in_force)
+    {
         this.time_in_force = time_in_force;
     }
 
-    public String getPost_only() {
+    public String getPost_only()
+    {
         return post_only;
     }
 
-    public void setPost_only(String post_only) {
+    public void setPost_only(String post_only)
+    {
         this.post_only = post_only;
     }
 
-    public String getCreated_at() {
+    public String getCreated_at()
+    {
         return created_at;
     }
 
-    public void setCreated_at(String created_at) {
+    public void setCreated_at(String created_at)
+    {
         this.created_at = created_at;
     }
 
-    public String getFill_fees() {
+    public String getFill_fees()
+    {
         return fill_fees;
     }
 
-    public void setFill_fees(String fill_fees) {
+    public void setFill_fees(String fill_fees)
+    {
         this.fill_fees = fill_fees;
     }
 
-    public String getFilled_size() {
+    public String getFilled_size()
+    {
         return filled_size;
     }
 
-    public void setFilled_size(String filled_size) {
+    public void setFilled_size(String filled_size)
+    {
         this.filled_size = filled_size;
     }
 
-    public String getExecuted_value() {
+    public String getExecuted_value()
+    {
         return executed_value;
     }
 
-    public void setExecuted_value(String executed_value) {
+    public void setExecuted_value(String executed_value)
+    {
         this.executed_value = executed_value;
     }
 
-    public String getStatus() {
+    public String getStatus()
+    {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(String status)
+    {
         this.status = status;
     }
 
-    public Boolean getSettled() {
+    public Boolean getSettled()
+    {
         return settled;
     }
 
-    public void setSettled(Boolean settled) {
+    public void setSettled(Boolean settled)
+    {
         this.settled = settled;
     }
 
-    public String toString() {
-        String orderString = getSide();
-        orderString += ": " + getProduct_id();
-        orderString += ": " + getPrice();
-        orderString += ": " + getSize();
-        return orderString;
+    @Override
+    public String toString()
+    {
+        return "Order{" +
+               getSide() +
+               " " +
+               getSize() +
+               " of " +
+               getProduct_id() +
+               " @" +
+               getPrice() +
+               '}';
     }
-
 }
