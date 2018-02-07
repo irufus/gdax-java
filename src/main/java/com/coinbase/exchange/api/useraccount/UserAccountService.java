06f2cef873593050bbb751039d219631842b8e94
Ishmael Rufus <sakamura@gmail.com>
2018-02-07 13:24:04 -0600

    Test created for issue #11 More private static fixing for member variables

--- src/main/java/com/coinbase/exchange/api/useraccount/UserAccountService.java
+++ src/main/java/com/coinbase/exchange/api/useraccount/UserAccountService.java
@@ -5,18 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
  * Created by robevansuk on 17/02/2017.
  */
 @Component
 public class UserAccountService {
 
-    static final String USER_ACCOUNT_ENDPOINT = "/users/self/trailing-volume";
+    private static final String USER_ACCOUNT_ENDPOINT = "/users/self/trailing-volume";
 
     @Autowired
     GdaxExchange gdaxExchange;
 
-    public UserAccountData getUserAccounts(){
-        return gdaxExchange.get(USER_ACCOUNT_ENDPOINT, new ParameterizedTypeReference<UserAccountData>() {});
+    /**
+     * Returns the 30 day trailing volume information from all accounts for the key provided
+     * @return UserAccountData
+     */
+    public List<UserAccountData> getTrailingVolume(){
+        return gdaxExchange.getAsList(USER_ACCOUNT_ENDPOINT, new ParameterizedTypeReference<UserAccountData[]>() {});
     }
 }
