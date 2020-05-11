656aae461a65cae87cb114701234be536734b87f
Sergey Ponomarev <stokito@gmail.com>
2020-05-11 15:12:00 +0300

    Build fix, update and cleanup dependencies (#60)
    * build.gradle: fix build
    
    Use https for repos. Gradle already have a macros for mavenCentral() and it's the same as repo.maven.apache.org/maven2.
    The repo1.maven.org/maven2/ is deprecated and in fact the same mavenCentral()
    
    * Update Spring Boot and Jackson
    
    * Remove JodaTime dependency
    
    Java 8 already have built-in date types
    
    * Remove GSon dependency
    
    Use Jackson ObjectMapper instead

--- src/main/java/com/coinbase/exchange/api/exchange/GdaxExchangeImpl.java
+++ src/main/java/com/coinbase/exchange/api/exchange/GdaxExchangeImpl.java
@@ -1,6 +1,7 @@
 package com.coinbase.exchange.api.exchange;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -109,8 +110,7 @@ public class GdaxExchangeImpl implements GdaxExchange {
 
     @Override
     public <T, R> T post(String resourcePath,  ParameterizedTypeReference<T> responseType, R jsonObj) {
-        Gson gson = new Gson();
-        String jsonBody = gson.toJson(jsonObj);
+        String jsonBody = toJson(jsonObj);
 
         try {
             ResponseEntity<T> response = restTemplate.exchange(getBaseUrl() + resourcePath,
@@ -159,5 +159,16 @@ public class GdaxExchangeImpl implements GdaxExchange {
 
         curlTest += "-X " + method + " " + getBaseUrl() + resource;
         log.debug(curlTest);
+    }
+
+    private String toJson(Object object) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(object);
+            return json;
+        } catch (JsonProcessingException e) {
+            log.error("Unable to serialize", e);
+            throw new RuntimeException("Unable to serialize");
+        }
     }
 }
