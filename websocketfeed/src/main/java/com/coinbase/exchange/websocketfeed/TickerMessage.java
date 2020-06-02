ae84231c2209dbe77339f918dc616f9aa5c28f42
robevans <robevansuk@users.noreply.github.com>
2020-06-02 16:13:42 +0100

    Fix TickerMessage definition and addition of Test to ensure its parsed correctly from Json

--- websocketfeed/src/main/java/com/coinbase/exchange/websocketfeed/TickerMessage.java
+++ websocketfeed/src/main/java/com/coinbase/exchange/websocketfeed/TickerMessage.java
@@ -1,43 +1,137 @@
 package com.coinbase.exchange.websocketfeed;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 /**
  * The ticker channel provides real-time price updates every time a match happens
  * <pre>
- *  {
- *   "type": "ticker",
- *   "trade_id": 8584621,
- *   "time": "2020-04-10T10:51:10.085085Z",
- *   "sequence": 9119799283,
- *   "product_id": "BTC-GBP",
- *   "price": "5587.84",
- *   "open_24h": "5900.24000000",
- *   "volume_24h": "1192.24218333",
- *   "low_24h": "5509.11000000",
- *   "high_24h": "5914.86000000",
- *   "volume_30d": "50369.34116944",
- *   "best_bid": "5581.72",
- *   "best_ask": "5587.84",
- *   "side": "buy",
- *   "last_size": "0.25926371"
+ * {
+ *     "type": "ticker",
+ *     "trade_id": 20153558,
+ *     "sequence": 3262786978,
+ *     "time": "2017-09-02T17:05:49.250000Z",
+ *     "product_id": "BTC-USD",
+ *     "price": "4388.01000000",
+ *     "side": "buy", // Taker side
+ *     "last_size": "0.03000000",
+ *     "best_bid": "4388",
+ *     "best_ask": "4388.01"
  * }
  * </pre>
  */
 public class TickerMessage extends FeedMessage {
-/*    Object sequence;
-    Object product_id;
-    Object price;
-    Object side;
-    Object time;
-    Object trade_id;*/
 
-    BigDecimal open_24h;
-    BigDecimal volume_24h;
-    BigDecimal low_24h;
-    BigDecimal high_24h;
-    BigDecimal volume_30d;
-    BigDecimal best_bid;
-    BigDecimal best_ask;
-    BigDecimal last_size;
+    private Long trade_id;
+    private Long sequence;
+    private Instant time;
+    private String product_id;
+    private BigDecimal price;
+    private String side;
+    private BigDecimal last_size;
+    private BigDecimal best_bid;
+    private BigDecimal best_ask;
+
+    public TickerMessage() {
+        setType("ticker");
+    }
+
+    public TickerMessage(Long trade_id,
+                         Long sequence,
+                         Instant time,
+                         String product_id,
+                         BigDecimal price,
+                         String side,
+                         BigDecimal last_size,
+                         BigDecimal best_bid,
+                         BigDecimal best_ask) {
+        this();
+        this.trade_id = trade_id;
+        this.sequence = sequence;
+        this.time = time;
+        this.product_id = product_id;
+        this.price = price;
+        this.side = side;
+        this.last_size = last_size;
+        this.best_bid = best_bid;
+        this.best_ask = best_ask;
+    }
+
+    public Long getTrade_id() {
+        return trade_id;
+    }
+
+    public void setTrade_id(Long trade_id) {
+        this.trade_id = trade_id;
+    }
+
+    @Override
+    public Long getSequence() {
+        return sequence;
+    }
+
+    @Override
+    public void setSequence(Long sequence) {
+        this.sequence = sequence;
+    }
+
+    @Override
+    public Instant getTime() {
+        return time;
+    }
+
+    @Override
+    public void setTime(Instant time) {
+        this.time = time;
+    }
+
+    @Override
+    public String getProduct_id() {
+        return product_id;
+    }
+
+    @Override
+    public void setProduct_id(String product_id) {
+        this.product_id = product_id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getSide() {
+        return side;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+
+    public BigDecimal getLast_size() {
+        return last_size;
+    }
+
+    public void setLast_size(BigDecimal last_size) {
+        this.last_size = last_size;
+    }
+
+    public BigDecimal getBest_bid() {
+        return best_bid;
+    }
+
+    public void setBest_bid(BigDecimal best_bid) {
+        this.best_bid = best_bid;
+    }
+
+    public BigDecimal getBest_ask() {
+        return best_ask;
+    }
+
+    public void setBest_ask(BigDecimal best_ask) {
+        this.best_ask = best_ask;
+    }
 }
